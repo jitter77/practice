1bbe0d477cd9334c2732a7baeeaac0e9b8bc3f8b
dp <auf_einem_auge_taub@yahoo.de>
2013-10-30 14:38:45 +0100

    modified:   ReadSerial.py

--- ReadSerial.py
+++ ReadSerial.py
@@ -1,8 +1,9 @@
 __author__ = 'dp'
 
 import serial
+baud = input("Set Baudrate: ")
 sCom1 =serial.Serial(0)
-sCom1.setBaudrate(115200)
+sCom1.setBaudrate(baud)
 if sCom1.isOpen()==False:
     sCom1.open()
 
