7eba4a9d8b4506c4f4ecc8a80113a3e4224004e4
dp <auf_einem_auge_taub@yahoo.de>
2013-11-01 11:25:02 +0100

    minor changes

--- ReadSerial.py
+++ ReadSerial.py
@@ -1,7 +1,16 @@
+"""Tool to read and write from/to /dev/tty"""
 __author__ = 'dp'
+version = 0.1
 
-import serial
+#TODO Versionscontrolle, Logdatei, check root
+import serial, os
+#TODO Liste oder Dictionary Baudrate implementieren, ? help
+Baudrate = {1:9600, 2:115200}
+for i in Baudrate:
+    print i
+
 baud = input("Set Baudrate: ")
+#TODO Liste oder Dictonary mit zugehoerigen Baudraten implementieren
 sCom1 =serial.Serial(0)
 sCom1.setBaudrate(baud)
 if sCom1.isOpen()==False:
@@ -9,6 +18,6 @@ if sCom1.isOpen()==False:
 
 while(1):
     line = sCom1.readline()
-    print line
+    print (line)
 sCom1.close()
 
