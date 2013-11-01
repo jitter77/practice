cbc9d5ceead9cf01f3a5383ff6bea1255f5f6022
dp <auf_einem_auge_taub@yahoo.de>
2013-11-01 21:01:00 +0100

    TODO

--- ReadSerial.py
+++ ReadSerial.py
@@ -1,14 +1,48 @@
+"""Tool to read and write from/to /dev/tty"""
 __author__ = 'dp'
+version = '0.1'
+#!/usr/bin/python
+#_*_ coding: utf-8 _*_
+#TODO Versionscontrolle, Logdatei, check root
+import serial, os, time
+#TODO Liste oder Dictionary Baudrate implementieren
+Baudrate = ["9600", "115200"]
+print ("Moegliche Baudraten: ")
+for i in Baudrate:
+    print (i)
 
-import serial
-baud = input("Set Baudrate: ")
-sCom1 =serial.Serial(0)
+baud = input("Baudrate waehlen: ")
+#TODO log, Funktion?
+log = raw_input("Logdatei erstellen? y or n")
+#log = str(log)
+if log in ['y', 'Y', 'ye', 'yes', 'Ye', 'Yes']:
+#TODO zeitstempel
+    logdatei = file("Systemlog", "w")
+    #logdatei = "Systemlog" and time
+else:
+    pass
+
+
+
+sCom1 =serial.Serial(port="/dev/ttyS0")
 sCom1.setBaudrate(baud)
+#Schnittstelle oeffnen
 if sCom1.isOpen()==False:
     sCom1.open()
-
-while(1):
+#TODO lesen zur Funktion umbauen?!
+while(1): #bis kein Inhalt mehr kommt
+    #von Schnittstelle lesen
     line = sCom1.readline()
-    print line
+    print (line)
+#Schnittstelle schliessen
 sCom1.close()
+#TODO schreiben zur Funktion umbauen?!
+while(): #bis Datei komplett gelesen
+    line = sCom1.writelines(lines) #lines: Auszulesende Zeilen
+    print(line)
+sCom1.close()
+
+
+
+
 
