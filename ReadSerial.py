063808e068e408bd8f04e36926338f676c5273dc
dp <auf_einem_auge_taub@yahoo.de>
2013-11-01 14:51:24 +0100

    minor changes

--- ReadSerial.py
+++ ReadSerial.py
@@ -1,23 +1,33 @@
 """Tool to read and write from/to /dev/tty"""
 __author__ = 'dp'
-version = 0.1
-
+version = '0.1'
+#!/usr/bin/python
+#_*_ coding: utf-8 _*_
 #TODO Versionscontrolle, Logdatei, check root
-import serial, os
-#TODO Liste oder Dictionary Baudrate implementieren, ? help
-Baudrate = {1:9600, 2:115200}
+import serial, os, time
+#TODO Liste oder Dictionary Baudrate implementieren
+Baudrate = ["9600", "115200"]
+print "Moegliche Baudraten: "
 for i in Baudrate:
     print i
 
-baud = input("Set Baudrate: ")
-#TODO Liste oder Dictonary mit zugehoerigen Baudraten implementieren
+baud = input("Baudrate waehlen: ")
+
 sCom1 =serial.Serial(0)
 sCom1.setBaudrate(baud)
+#Schnittstelle oeffnen
 if sCom1.isOpen()==False:
     sCom1.open()
 
-while(1):
+while():
+    #von Schnittstelle lesen
     line = sCom1.readline()
     print (line)
+#Schnittstelle schliessen
 sCom1.close()
+
+
+
+
+
 
