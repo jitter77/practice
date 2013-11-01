63d5c9c79a732b1063864adff9f80a9a5385f0d1
dp <auf_einem_auge_taub@yahoo.de>
2013-11-01 16:05:29 +0100

    time, logfile

--- ReadSerial.py
+++ ReadSerial.py
@@ -12,21 +12,34 @@ for i in Baudrate:
     print i
 
 baud = input("Baudrate waehlen: ")
+#TODO log, Funktion?
+log = raw_input("Logdatei erstellen? y or n")
+#log = str(log)
+if log in ['y', 'Y', 'ye', 'yes', 'Ye', 'Yes']:
+    logdatei = file("Systemlog", "w")
+    #logdatei = "Systemlog" and time
+else:
+    pass
 
-sCom1 =serial.Serial(0)
+
+
+sCom1 =serial.Serial(port="/dev/ttyS0")
 sCom1.setBaudrate(baud)
 #Schnittstelle oeffnen
 if sCom1.isOpen()==False:
     sCom1.open()
-
-while():
+#TODO lesen zur Funktion umbauen?!
+while(1): #bis kein Inhalt mehr kommt
     #von Schnittstelle lesen
     line = sCom1.readline()
     print (line)
 #Schnittstelle schliessen
 sCom1.close()
-
-
+#TODO schreiben zur Funktion umbauen?!
+while(): #bis Datei komplett gelesen
+    line = sCom1.writelines(lines) #lines: Auszulesende Zeilen
+    print(line)
+sCom1.close()
 
 
 
