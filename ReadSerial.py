f891041fabfb908a2491dbf32c621991a3aa9fbb
dp <auf_einem_auge_taub@yahoo.de>
2013-11-02 01:10:16 +0100

    cleanup

--- ReadSerial.py
+++ ReadSerial.py
@@ -1,11 +1,17 @@
-"""Tool to read and write from/to /dev/tty"""
+# coding=utf-8
+"""Tool to read and write from/to /dev/tty, write log if wanted"""
 __author__ = 'dp'
 version = '0.1'
+
 #!/usr/bin/python
 #_*_ coding: utf-8 _*_
-#TODO Versionscontrolle, Logdatei, check root
+
+#TODO Versionscontrolle, Logdatei, check root, http impelmentieren
+
 import serial, os, time
+
 #TODO Liste oder Dictionary Baudrate implementieren
+
 Baudrate = ["9600", "115200"]
 print ("Moegliche Baudraten: ")
 for i in Baudrate:
@@ -23,13 +29,16 @@ if log in ['y', 'Y', 'ye', 'yes', 'Ye', 'Yes']:
     print time.strftime("%H:%M:%S")
     test = str(time.strftime("%H:%M:%S"))
     print test
+
 #TODO get_time Funktion
 def get_time(time):
+    """Zeit im Format Stunde/Minute/Sekunde auslesen und zurückgeben"""
     pass
-
 else:
     pass
+
 def write_log(data):
+    """Logdatei öffnen, Zeilennummer und Zeitstempel einfügen, Zeilen schreiben, Datei schließen"""
     datei = open(logdatei, "w+")
     print(data)
     datei.close()
@@ -40,14 +49,18 @@ sCom1.setBaudrate(baud)
 #Schnittstelle oeffnen
 if sCom1.isOpen()==False:
     sCom1.open()
+
 #TODO lesen zur Funktion umbauen?!
+
 while(1): #bis kein Inhalt mehr kommt
     #von Schnittstelle lesen
     line = sCom1.readline()
     print (line)
 #Schnittstelle schliessen
 sCom1.close()
+
 #TODO schreiben zur Funktion umbauen?!
+
 while(): #bis Datei komplett gelesen
     line = sCom1.writelines(lines) #lines: Auszulesende Zeilen
     print(line)
