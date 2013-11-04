d614602959a985be8e12f988af41f5866758ec43
dp <auf_einem_auge_taub@yahoo.de>
2013-11-04 15:00:20 +0100

    cleanup

--- ReadSerial.py
+++ ReadSerial.py
@@ -1,24 +1,9 @@
 # coding=utf-8
 """Tool to read and write from/to /dev/tty, write log if wanted"""
 __author__ = 'dp'
-
-#TODO Versionscontrolle, Logdatei, check root
-import serial
-#TODO Liste oder Dictionary Baudrate implementieren, ? help
-baud = input("Set Baudrate: ")
-#TODO Liste oder Dictonary mit zugehörigen Baudraten implementieren
-sCom1 =serial.Serial(0)
-sCom1.setBaudrate(baud)
-if sCom1.isOpen()==False:
-    sCom1.open()
-
-while(1):
-    line = sCom1.readline()
-    print (line)
-sCom1.close()
-
 version = '0.1'
 version_web = '0'
+Port = "/dev/ttyS0"
 #!/usr/bin/python
 #_*_ coding: utf-8 _*_
 
@@ -37,22 +22,9 @@ import serial
 if platform.system() != "Linux":
     print ("Only for Linux (so far)!")
 
-version = '0.1'
-
-#!/usr/bin/python
-#_*_ coding: utf-8 _*_
-
-#TODOLogdatei, check root, http impelmentieren
-
-import serial, os, time
-
-#TODO Liste oder Dictionary Baudrate implementieren
-
-
 #Check ob Programm mit root - Rechten aufgerufen wurde
 user = os.geteuid()
 if user != 0:
-
     print "You have to be root! Start program as root or using sudo!\nExiting now."
     quit()
 
@@ -76,10 +48,6 @@ def first_run():
 Baudrate = ["9600", "115200"]
 Module = ["TX25", "TX28", "TX28S", "TX48", "TX53", "TX6DL", "TX6Q"]
 OS = ["WinCE6", "WinEC7", "Android"]
-
-
-Baudrate = ["9600", "115200"]
-
 print ("Moegliche Baudraten: ")
 for i in Baudrate:
     print (i)
@@ -88,7 +56,6 @@ baud = input("Baudrate waehlen: ")
 #TODO log, Funktion?
 log = raw_input("Logdatei erstellen? y or n: ")
 #log = str(log)
-
 if log in ['y', 'Y', 'ye', 'yes', 'Ye', 'Yes', 'YES', 'YE']:
 #TODO zeitstempel
     #Datei = ("Systemlog"+(time.strftime("%h:%m:%s"), "w"))
@@ -111,10 +78,9 @@ def write_log(data):
     datei.close()
 
 
-
-def open_com():
+def open_com(sCom1=Port):
     """ComPort oeffnen"""
-    sCom1 = serial(port="/dev/ttyS0")
+    #sCom1 = serial(port="/dev/ttyS0")
     sCom1.setBaudrate(baud)
     if sCom1.isOpen()==False:
         sCom1.open()
@@ -126,7 +92,7 @@ def open_com():
  #   sCom1.open()
 
 #TODO lesen zur Funktion umbauen?!
-def read_com():
+def read_com(sCom1=Port):
     """Com oeffnen, lesen bis keien Zeichen mehr kommen, in logdatei schreiben, Schnittstelle schliessen"""
     open_com()
     while():
@@ -148,36 +114,9 @@ def read_com():
   #  print(line)
 #sCom1.close()
 
-def write_com():
+def write_com(sCom1=Port):
     """Com oeffnen, Daten einlesen bis kein Input, Daten schreiben, Com schliessen"""
     open_com()
     while():
         line = sCom1.write(data=test)
         sCom1.close()
-
-
-
-sCom1 =serial.Serial(port="/dev/ttyS0")
-sCom1.setBaudrate(baud)
-#Schnittstelle oeffnen
-if sCom1.isOpen()==False:
-    sCom1.open()
-
-#TODO lesen zur Funktion umbauen?!
-
-while(1): #bis kein Inhalt mehr kommt
-    #von Schnittstelle lesen
-    line = sCom1.readline()
-    print (line)
-#Schnittstelle schliessen
-sCom1.close()
-
-#TODO schreiben zur Funktion umbauen?!
-
-while(): #bis Datei komplett gelesen
-    line = sCom1.writelines(lines) #lines: Auszulesende Zeilen
-    print(line)
-sCom1.close()
-
-
-
