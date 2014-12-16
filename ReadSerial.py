b8415a6320bf027d39a566a2d5cf7b6521c61479
dp <auf_einem_auge_taub@yahoo.de>
2014-12-16 13:35:14 +0100

    minor changes
    just some clean-up, mostly in comments

--- ReadSerial.py
+++ ReadSerial.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python2.7
 # _*_ coding: utf-8 _*_
 """
-\Tool to read and write from/to /dev/tty, write log if wanted
+Tool to read and write from/to /dev/tty, write log if wanted
 """
 __author__ = 'dp'
 if __name__ == 'main':
@@ -9,17 +9,17 @@ if __name__ == 'main':
 
 __version__ = '0.1'
 
-#TODO wiederkehrende Teile als Klasse anlegen
-#TODO Logdatei - Send - Receive -Timestamp
-#TODO Lokal arbeiten oder Images herunterladen
-#TODO Erstinstallation, defaults (Sprache, Pfade) speichern
-#TODO Proxy?
-#TODO GIT PULL!!!
-#TODO freie IPs abfragen eigene: ifconfig | grep inet
-#TODO ENV files anlegen
-#TODO Übergabewerte (data, ausgelesenes Environment)
-#TODO weiteres Modul flashen?
-#TODO Konfiguration vorhanden, neu?
+# TODO wiederkehrende Teile als Klasse anlegen
+# TODO Logdatei - Send - Receive -Timestamp
+# TODO Lokal arbeiten oder Images herunterladen
+# TODO Erstinstallation, defaults (Sprache, Pfade) speichern
+# TODO Proxy?
+# TODO GIT PULL!!!
+# TODO freie IPs abfragen eigene: ifconfig | grep inet
+# TODO ENV files anlegen
+# TODO Übergabewerte (data, ausgelesenes Environment)
+# TODO weiteres Modul flashen?
+# TODO Konfiguration vorhanden, neu?
 
 import os
 import sh
@@ -37,19 +37,19 @@ version_pickle = pickle.HIGHEST_PROTOCOL
 Baudrate = ["9600", "19200", "38400", "57600", "115200"]
 
 
-#tree = ['/files_flasher/modules/tx25/os/linux/', '/files_flasher/modules/tx28/os/linux/']
-#Wurde unter Linux gestartet?
+# tree = ['/files_flasher/modules/tx25/os/linux/', '/files_flasher/modules/tx28/os/linux/']
+# Wurde unter Linux gestartet?
 if platform.system() != "Linux":
     print "-" * 40
     print ("| Only for Linux (so far)! |")
     print ("| Exiting now!             |")
     print "-" * 40
-    #3 Sekunden warten
+    # 3 Sekunden warten
     time.sleep(3)
-    #und tschüss
+    # und tschüss
     quit()
 
-#Check, ob Programm mit root - Rechten aufgerufen wurde
+# Check, ob Programm mit root - Rechten aufgerufen wurde
 user = os.geteuid()
 if user != 0:
     print "-" * 40
@@ -57,12 +57,12 @@ if user != 0:
     print "|Start program as root or using sudo!  |"
     print "|Exiting now.", " " * 24, "|"
     print "-" * 40
-    #3 Sekunden warten
+    # 3 Sekunden warten
     time.sleep(3)
     # und tschüss
     quit()
 
-#check for update
+# check for update
 print"-" * 22
 print("| Check for updates? |")
 print"-" * 22
@@ -87,30 +87,30 @@ if update in ['y', 'Y', 'ye', 'yes', 'Ye', 'Yes', 'YES', 'YE']:
 else:
     pass
 
-#Verfügbare Ports anzeigen, Auswahl treffen, speichern
-#TODO  in first run eingliedern, ansonsten gespeicherten Port behalten. Vorher gespeicherte Konfiguration anzeigen
+# Verfügbare Ports anzeigen, Auswahl treffen, speichern
+# TODO  in first run eingliedern, ansonsten gespeicherten Port behalten. Vorher gespeicherte Konfiguration anzeigen
 print"-" * 26
 print "-" * 26
 print("| List of available ports|\n| Please choose port     |")
 print "-" * 26
-#Aufruf extern list_ports
+# Aufruf extern list_ports
 list_ports.main()
 port = raw_input("Please enter your port like this: /dev/ttyS0\n")
 print("Chosen port: "), port
 
 
-#Erstinstallation durchführen, erneuern
+# Erstinstallation durchführen, erneuern
 print("Programm einrichten?\n")
 install = raw_input("y/n\n")
 if install == "y":
     pass
-    #TODO first run aufrufen
+    # TODO first run aufrufen
 else:
     print("no first run")
     pass
 
 
-#auslagern in first run!
+# auslagern in first run!
 def ip_adresses():
     """
     IP Host und Device auslesen bzw setzen
@@ -120,13 +120,13 @@ def ip_adresses():
     print check
 
 
-#TODO first run auslagern
+# TODO first run auslagern
 def first_run(baud, version, version_flash):
     """
     first run, conf erstellen, werte eintragen
     """
     heim = os.getenv("HOME")
-    #TODO Version in settings.conf einfügen! Für aktuellere Downloads! Oder doch nicht: git pull!
+    # TODO Version in settings.conf einfügen! Für aktuellere Downloads! Oder doch nicht: git pull!
     try:
         if os.path.exists(heim + "/files_flasher/settings.conf " + version_flash):
             pass
@@ -135,22 +135,22 @@ def first_run(baud, version, version_flash):
 
             for zeile in tree:
                 os.makedirs(heim + zeile)
-                #file(heim+"/files_flasher/settings.conf")
+                # file(heim+"/files_flasher/settings.conf")
         with open(heim + "/files_flasher/settings" + version_flash + ".conf", 'wb') as datei:
             pickle.dump("test", datei, protocol=version_pickle)
-            #datei.close()
+            # datei.close()
     except IOError:
         print ("IOError!")
 
-#Vielleicht mal den folgenden Rattenschwanz als Funktion mit Dictionary vereinfachen?
+# Vielleicht mal den folgenden Rattenschwanz als Funktion mit Dictionary vereinfachen?
 print ("\nMoegliche Baudraten:\n(Bitte Zahl zw. 1 und 5 wählen) ")
 print "-" * 32
 n = 1
 for i in Baudrate:
     print n, ":", (i)
     n += 1
-    #print (i)
-#first_run(baud=Baudrate, Version=version_pickle, version_flash=version)
+    # print (i)
+# first_run(baud=Baudrate, Version=version_pickle, version_flash=version)
 select = input("\nBaudrate wählen: ")
 if select == 1:
     baud = 9600
@@ -168,7 +168,7 @@ else:
     time.sleep(3)
     quit()
 
-#Modul auswählen
+# Modul auswählen
 choose_CoM.main()
 
 
@@ -205,12 +205,12 @@ class Flash:
             print (line)
         port.close()
 
-        #while(1): #bis kein Inhalt mehr kommt
-        #von Schnittstelle lesen
+        # while(1): # bis kein Inhalt mehr kommt
+        # von Schnittstelle lesen
         #   line = sCom1.readline()
         #  print (line)
-        #Schnittstelle schliessen
-        #sCom1.close()
+        # Schnittstelle schliessen
+        # sCom1.close()
 
     def write_log(self, logdatei, data):
         """
@@ -220,7 +220,7 @@ class Flash:
         print(data)
         datei.close()
 
-#TODO log, Funktion? Ja! Wegen Rückprung bei Falscheingabe!
+# TODO log, Funktion? Ja! Wegen Rückprung bei Falscheingabe!
 log = raw_input("Logdatei erstellen? y or n: ")
 
 if log == "y":
@@ -245,23 +245,23 @@ def open_com(port):
     """
     ComPort oeffnen port
     """
-    #sCom1 = serial(port="/dev/ttyS0")
+    # sCom1 = serial(port="/dev/ttyS0")
     port.setBaudrate(baud)
     if port.isOpen() == False:
         port.open()
 
-        #sCom1 =serial.Serial(port="/dev/ttyS0")
-        #sCom1.setBaudrate(baud)
-        #Schnittstelle oeffnen
-        #if sCom1.isOpen()==False:
+        # sCom1 =serial.Serial(port="/dev/ttyS0")
+        # sCom1.setBaudrate(baud)
+        # Schnittstelle oeffnen
+        # if sCom1.isOpen()==False:
         #   sCom1.open()
 
-        #while(): #bis Datei komplett gelesen
+        # while(): #bis Datei komplett gelesen
         #   line = sCom1.writelines(lines) #lines: Auszulesende Zeilen
         #  print(line)
 
 
-#sCom1.close()
+# sCom1.close()
 
 
 def write_com(data, port):
@@ -269,16 +269,17 @@ def write_com(data, port):
     Com oeffnen, Daten einlesen bis kein Input, Daten schreiben, Com schliessen
     """
     open_com(port)
-    #for zeile in datei:
+    # for zeile in datei:
 
     while ():
         line = port.write(data=test)
         port.close()
 
 
-#weglassen? option?
+# weglassen? option?
 def run_tftp():
     """
     tftp Server starten
     """
     os.popen("/etc/init.d/tftpd-hpa restart")
+    # FIXME
